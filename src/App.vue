5665addad6ed4b40d51a4c28c407690d69c9841e
Santosh Managuli <santoshmanaguli98@gmail.com>
2025-02-10 02:28:16 +0530

    binding the components to the main page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
-<script></script>
+<script>
+import Navbar from './components/Navbar/Navbar.vue'
+import Breadcrumb from './components/Breadcrumb/Breadcrumb.vue'
+import Home from './components/Home/Home.vue'
 
+export default {
+  components: {
+    Navbar,
+    Breadcrumb,
+    Home,
+  },
+}
+</script>
 <template>
-  <h1>WElcome</h1>
+  <div>
+    <Navbar />
+    <Breadcrumb />
+    <Home />
+  </div>
 </template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+* {
+  font-family: 'Poppins';
+}
+body {
+  background-color: #f7f8fb;
+}
+
+.font-weight500 {
+  font-weight: 500;
+}
+
+.bell-icon {
+  width: 20px;
+  height: 20px;
+  color: #fff;
+  border-radius: 50%;
+}
+</style>
